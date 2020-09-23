2a063de697686b772859d61e41426b0a6f2c7c19
pyl <a522723936@gmail.com>
2020-09-23 19:03:31 +1000

    Define the models for User and Task and their relationship

---
+++ app/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $guarded = [];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y/m/d',
+        'updated_at' => 'datetime:Y/m/d',
+    ];
+    /**
+     * Get the owner of the task
+     *
+     * @return void
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
